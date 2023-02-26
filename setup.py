3bcb4e181d84732fb2ba2a20ee4dd265381b0ff3
Htet Aung Kyaw <98970244+hanklokyaw@users.noreply.github.com>
2023-02-25 16:44:29 -0800

    setup script modified

--- setup.py
+++ setup.py
@@ -2,11 +2,6 @@ from setuptools import setup, find_packages
 import codecs
 import os
 
-here = os.path.abspath(os.path.dirname(__file__))
-
-with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
-    long_description = "\n" + fh.read()
-
 VERSION = '0.1.0'
 DESCRIPTION = 'A templates module for business analytics peers'
 LONG_DESCRIPTION = 'A templates module for business analytics peers. The structures and'\
